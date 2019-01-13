663b6e67d4d1f43f975574fc1194a2231f9373a6
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-13 09:06:42 +0800

    +Star

---
+++ AsteroidsGame/Star.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+//Брижак Андрей Домашнее задание по курсу C# уровень 2 урок 4
+
+
+namespace AsteroidsGame
+{
+    /// <summary>
+    /// класс Star, наследуемый от BaseObject
+    /// </summary>
+    class Star : BaseObject
+    {
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+
+        }
+
+        /// <summary>
+        /// рисует обьект Star
+        /// </summary>
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawLine(Pens.White, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height);
+            Game.Buffer.Graphics.DrawLine(Pens.White, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
+        }
+
+        /// <summary>
+        /// рассчитывает местоположение обьекта Star
+        /// </summary>
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
+        }
+    }
+}
