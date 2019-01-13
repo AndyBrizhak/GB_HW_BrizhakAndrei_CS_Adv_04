abe6fa75e105c8689dafdb5d0ab9507897eaf29c
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-13 09:01:56 +0800

    +    class Program

--- AsteroidsGame/Program.cs
+++ AsteroidsGame/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Forms;
+//Брижак Андрей Домашнее задание по курсу C# уровень 2 урок 4
 
 namespace AsteroidsGame
 {
@@ -10,6 +7,25 @@ namespace AsteroidsGame
     {
         static void Main(string[] args)
         {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            //  Game.Init(form);
+            //  form.Show();
+            //  Game.Draw();
+            //  Application.Run(form);
+
+            // Form form = new Form
+            // {
+            //     Width = Screen.PrimaryScreen.Bounds.Width,
+            //     Height = Screen.PrimaryScreen.Bounds.Height
+            // };
+
+            Game.Init(form);
+            form.Show();
+            Game.Load();
+            Game.Draw();
+            Application.Run(form);
         }
     }
 }
