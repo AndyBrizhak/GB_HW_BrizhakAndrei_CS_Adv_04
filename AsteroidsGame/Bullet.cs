90499460079b10dd4482722ad21d577dc5ae3d1d
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-13 08:36:41 +0800

    +Bullet

---
+++ AsteroidsGame/Bullet.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+//Брижак Андрей Домашнее задание по курсу C# уровень 2 урок 4
+
+namespace AsteroidsGame
+{
+    /// <summary>
+    /// класс Bullet, наследуемый от BaseObject
+    /// </summary>
+    class Bullet : BaseObject
+    {
+        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        /// <summary>
+        /// Событие (делегат) уничтожение Bullet
+        /// </summary>
+        public static event Message MessageBulletDestroyed;
+
+        /// <summary>
+        /// Прорисовка пули
+        /// </summary>
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawRectangle(Pens.OrangeRed, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        /// <summary>
+        /// Движение пули
+        /// </summary>
+        public override void Update()
+        {
+            Pos.X = Pos.X + 3;
+        }
+
+
+        /// <summary>
+        /// Разрушение пули
+        /// </summary>
+        public void MessageDestroyed()
+        {
+            MessageBulletDestroyed?.Invoke(); // Когда пуля разрушается вызываем это событие
+        }
+
+        /// <summary>
+        /// при столкновении перенести положение снаряда на левый кран экрана
+        /// </summary>
+        public void Crash()
+        {
+            Pos.X = 0;
+        }
+
+        /// <summary>
+        /// Вывод сообщения в консоль о разрушении пули
+        /// </summary>
+        public static void ShowMessageBulletDestroyed()
+        {
+            Console.WriteLine("Bullet destroeyd");
+            Game.MessageToFile("Bullet destroeyd");
+        }
+    }
+}
