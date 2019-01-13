f81d2e4cc10ecd1ee2471fab8921c39639ed5f0e
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-13 16:26:20 +0800

    + проект с решением задачи ДЗ по подсчеты встречаемости элемента в коллекции целых чисел

---
+++ Task02a_GenericCollectionInt/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+//Брижак Андрей Домашняя работа 4 C# продвинутый
+//Дана коллекция List<T>, требуется подсчитать, сколько раз каждый элемент встречается в
+//данной коллекции.
+
+namespace Task02a_GenericCollectionInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listiInts = new List<int>(); 
+            Random Rnd = new Random();
+            for (int i = 0; i < 100; i++)
+                listiInts.Add(Rnd.Next(0, 100));
+
+            var aFindPairs = FindPairs(listiInts) ?? throw new ArgumentNullException("FindPairs(listiInts)");
+
+            Dictionary<int, int> FindPairs(List<int> list)
+            {
+                Dictionary<int, int> resInts = new Dictionary<int, int>();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (resInts.ContainsKey(list[i]))
+                    {
+                        resInts[list[i]]++;
+                    }
+                    else
+                    {
+                        resInts.Add(list[i],1);
+                    }
+                   
+                }
+
+                return resInts;
+            }
+
+            foreach (var item in aFindPairs)
+            {
+               System.Console.WriteLine(item); 
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+        
+    }
+}
