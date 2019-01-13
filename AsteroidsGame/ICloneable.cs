50d7f199f0312a5c9f2cc2324b604cdf6dc8f43b
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-13 08:57:27 +0800

    +ICloneable

---
+++ AsteroidsGame/ICloneable.cs
@@ -0,0 +1,11 @@
+﻿namespace AsteroidsGame
+{
+    interface ICloneable
+    {
+        /// <summary>
+        ///   Создает новый объект, являющийся копией текущего экземпляра.
+        /// </summary>
+        /// <returns>Новый объект, являющийся копией этого экземпляра.</returns>
+        object Clone();
+    }
+}
