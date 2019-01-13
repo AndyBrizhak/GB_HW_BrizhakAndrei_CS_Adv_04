de151e4e52fa4a7c73606490bb15072253a904b4
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-13 17:38:28 +0800

    +задача на обобщенну коллекцию

---
+++ Task2b_GenericCollection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2b_GenericCollection
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    T string str = new T();
+
+
+        //    //foreach (var item in aFindPairs)
+        //    //{
+        //    //    System.Console.WriteLine(item);
+        //    //}
+        //    //Console.ReadKey();
+        //}
+
+        
+    }
+
+    public class T
+    {
+        Dictionary<T, int> FindPairs(List<T> list)
+        {
+            Dictionary<T, int> res = new Dictionary<T, int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (res.ContainsKey(list[i]))
+                {
+                    res[list[i]]++;
+                }
+                else
+                {
+                    res.Add(list[i], 1);
+                }
+            }
+
+            return res;
+        }
+    }
+}
